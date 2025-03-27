26ac7358193946eb2361efebe17d22ebd5e76df5
Justina77 <petrushinajustina@gmail.com>
2025-03-27 14:33:15 +0200

    Audio

---
+++ My project (2)/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource source;
+    [SerializeField] private AudioClip clip;
+
+    private void OnEnable()
+    {
+        PlayerEvents.onHitEvent += PlayHitSound;
+    }
+
+    private void OnDisable()
+    {
+        PlayerEvents.onHitEvent -= PlayHitSound;
+    }
+
+    private void PlayHitSound()
+    {
+        source.PlayOneShot(clip);
+    }
+}
