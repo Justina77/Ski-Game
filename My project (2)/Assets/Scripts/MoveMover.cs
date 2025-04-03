20e9a131e48570cf18b608753c289f2e1dbcec63
Justina77 <petrushinajustina@gmail.com>
2025-04-03 15:41:42 +0300

    7

--- My project (2)/Assets/Scripts/MoveMover.cs
+++ My project (2)/Assets/Scripts/MoveMover.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class MoveMover : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         for(int i = 0; i < 150000; i++)
