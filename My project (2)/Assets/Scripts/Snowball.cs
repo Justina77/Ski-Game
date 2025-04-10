b3c7a59c0c3b0ed86b4c9207938475cedda7530b
Justina77 <petrushinajustina@gmail.com>
2025-04-10 13:18:45 +0300

    Snowball

---
+++ My project (2)/Assets/Scripts/Snowball.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Snowball : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
